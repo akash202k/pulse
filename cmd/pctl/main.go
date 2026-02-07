705f8b6c528bdc63271e290467e95a0c4bfa71d1
akash202k <akashpawar202k@gmail.com>
2026-02-07 19:05:41 +0530

    load pulse.yaml config

--- cmd/pctl/main.go
+++ cmd/pctl/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/akash202k/pulse/internal/config"
+)
 
 func main() {
 	fmt.Println("Pulse CLI starting...")
+	cfg, err := config.Load("configs/pulse.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Loaded %d Services\n", len(cfg.Services))
+	fmt.Printf("Config: %+v\n", cfg)
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,27 @@
 package config
 
-import "github.com/akash202k/pulse/internal/model"
+import (
+	"os"
+
+	"github.com/akash202k/pulse/internal/model"
+	"gopkg.in/yaml.v3"
+)
 
 type Config struct {
 	Services []model.Service `yaml:"services"`
 	SLO      model.SLO       `yaml:"SLO"`
+}
+
+func Load(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
 }
